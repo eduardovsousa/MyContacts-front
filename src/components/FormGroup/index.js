3409e78225577094e0d6283892802afc5222fc6f
Eduardo Sousa <eduardo.sousa@17com.com.br>
2024-03-15 12:22:55 -0300

    refact: create loader component

--- src/components/FormGroup/index.js
+++ src/components/FormGroup/index.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 
+import Spinner from '../Spinner';
 import { Container } from './styles';
 
 export default function FormGroup({ children, error, isLoading }) {
@@ -8,7 +9,11 @@ export default function FormGroup({ children, error, isLoading }) {
       <div className="form-item">
         {children}
 
-        {isLoading && <div className="loader" />}
+        {isLoading && (
+          <div className="loader">
+            <Spinner size={16} />
+          </div>
+        )}
       </div>
       {error && <small>{error}</small>}
     </Container>
